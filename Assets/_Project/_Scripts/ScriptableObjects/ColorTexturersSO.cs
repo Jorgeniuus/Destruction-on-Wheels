ab1a3a831fbe38b5fc5049e6b92c97084172d30f
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2024-01-21 12:38:26 -0300

    feat: working on implementation car customization

---
+++ Assets/_Project/_Scripts/ScriptableObjects/ColorTexturersSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Painting", menuName = "ScriptableObjects/painting", order = 1)]
+public class ColorTexturersSO : ScriptableObject
+{
+    public Color color;
+}
+
+public struct ColorTexture
+{
+
+}
