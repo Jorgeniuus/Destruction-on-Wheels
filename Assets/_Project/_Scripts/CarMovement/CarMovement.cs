689e90dfe616bde14809e7128d594192ce09814f
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2024-01-20 22:08:58 -0300

    feat: added car movement mechanic

--- Assets/_Project/_Scripts/CarMovement/CarMovement.cs
+++ Assets/_Project/_Scripts/CarMovement/CarMovement.cs
@@ -4,12 +4,76 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
+    [Header("Vehicle Settings")] 
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
+
+    [Header("Wheels Settings")]
+    [SerializeField] private WheelCollider[] frontWheelCollider;
+    [SerializeField] private WheelCollider[] rearWheelCollider;
+    [SerializeField] private Transform[] frontWheelTransform;
+    [SerializeField] private Transform[] rearWheelTransform;
+
+    private bool _isBreaking;
+    private float _horizontal;
+    private float _vertical;
+
     private void Update()
     {
-        
+        InputMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        MotorManager();
+        SteeringManager();
+        WheelsUpdate();
+    }
+
+    private void InputMovement()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+
+        _isBreaking = Input.GetKey(KeyCode.Space);
+    }
+    private void MotorManager()
+    {
+        foreach (WheelCollider wheels in rearWheelCollider) wheels.motorTorque = _vertical * motorForce;
+        foreach (WheelCollider wheels in frontWheelCollider) wheels.motorTorque = _vertical * motorForce;
+        float currentBreaking = _isBreaking ? breakForce : 0f;
+        BreakingManager(currentBreaking);
+    }
+
+    private void BreakingManager(float currentBreaking)
+    {
+        for (int i = 0; i < rearWheelCollider.Length; i++)
+        {
+            rearWheelCollider[i].brakeTorque = currentBreaking;
+            frontWheelCollider[i].brakeTorque = currentBreaking;
+        }
+    }
+
+    private void SteeringManager()
+    {
+        foreach (WheelCollider wheels in frontWheelCollider) wheels.steerAngle = _horizontal * maxSteerAngle;
+    }
+
+    private void WheelsUpdate()
+    {
+        for (int i = 0; i < frontWheelCollider.Length; i++)
+        {
+            WheelsTransformUpdate(frontWheelCollider[i], frontWheelTransform[i]);
+            WheelsTransformUpdate(rearWheelCollider[i], rearWheelTransform[i]);
+        }
+    }
+    private void WheelsTransformUpdate(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 position;
+        Quaternion rotation;
+
+        wheelCollider.GetWorldPose(out position, out rotation);
+        wheelTransform.rotation = rotation;
     }
 }
